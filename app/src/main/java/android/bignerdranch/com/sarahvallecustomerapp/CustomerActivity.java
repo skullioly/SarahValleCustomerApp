f2b8d79a2f197818045ea9dc0287eb382974e202
Sarah <skulli.oly@gmail.com>
2018-08-27 22:43:53 -0400

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/com/sarahvallecustomerapp/CustomerActivity.java
@@ -0,0 +1,13 @@
+package android.bignerdranch.com.sarahvallecustomerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+    }
+}
---
+++ app/src/main/java/android/bignerdranch/com/sarahvallecustomerapp/SessionActivity.java
@@ -0,0 +1,13 @@
+package android.bignerdranch.com.sarahvallecustomerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+    }
+}
