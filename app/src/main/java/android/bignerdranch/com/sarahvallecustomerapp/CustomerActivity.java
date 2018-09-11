9cc075e867dc7a06abd73a1ebd24068eb03e3782
Sarah <skulli.oly@gmail.com>
2018-09-10 20:48:07 -0400

    Changes for assignment 4.

--- app/src/main/java/android/bignerdranch/com/sarahvallecustomerapp/CustomerActivity.java
+++ app/src/main/java/android/bignerdranch/com/sarahvallecustomerapp/CustomerActivity.java
@@ -1,7 +1,12 @@
 package android.bignerdranch.com.sarahvallecustomerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomerActivity extends AppCompatActivity {
 
@@ -9,5 +14,57 @@ public class CustomerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        configureDisplayButton();
+        configureNewCustomerButton();
+        configuresessionManagerButton();
+        configurepaymentButton();
     }
+
+    private void configurepaymentButton() {
+        Button paymentButton = (Button) findViewById(R.id.paymentButton);
+        paymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustomerActivity.this, PaymentActivity.class));
+            }
+        });
+    }
+
+    private void configuresessionManagerButton() {
+        Button sessionManagerButton = (Button) findViewById(R.id.sessionManagerButton);
+        sessionManagerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustomerActivity.this, SessionActivity.class));
+            }
+        });
+    }
+
+    private void configureNewCustomerButton() {
+        Button newCustomerButton = (Button) findViewById(R.id.newCustomerButton);
+        newCustomerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustomerActivity.this, NewActivity.class));
+            }
+        });
+    }
+
+    private void configureDisplayButton(){
+        Button displayButton = (Button) findViewById(R.id.displayButton);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CustomerActivity.this, DisplayActivity.class));
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.fragment_logoff, menu);
+        return true;
+    }
+
 }
--- app/src/main/java/android/bignerdranch/com/sarahvallecustomerapp/SessionActivity.java
+++ app/src/main/java/android/bignerdranch/com/sarahvallecustomerapp/SessionActivity.java
@@ -1,7 +1,10 @@
 package android.bignerdranch.com.sarahvallecustomerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SessionActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class SessionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
+
+        configurenewSessionButton();
+        configurecompleteSessionButton();
+    }
+
+    private void configurecompleteSessionButton() {
+        Button completeSessionButton = (Button) findViewById(R.id.completeSessionButton);
+        completeSessionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SessionActivity.this, CompleteSessionActivity.class));
+            }
+        });
+    }
+
+    private void configurenewSessionButton() {
+        Button newSessionButton = (Button) findViewById(R.id.newSessionButton);
+        newSessionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SessionActivity.this, NewSessionActivity.class));
+            }
+        });
     }
 }
